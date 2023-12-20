4d28691b6f7b48d3f13f9588486c2acc423ca1a9
Laura Segouras <lausegouras@gmail.com>
2023-12-20 11:20:56 -0300

    some general styles fixes, register styles and js

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -2,26 +2,51 @@ import styles from "./Register.module.css"
 import { useState, useEffect } from 'react'
 
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [error, setError] = useState("")
+  
+  const handleSubmit = (e) => {
+    e.prevent.default()
+
+    setError("")
+
+    const user = {
+      name,
+      email,
+      password
+    }
+
+    if (password !== confirmPassword) {
+      setError("Passwords must match!")
+      return;
+    }
+
+    console.log(user);
+  }
+  
   return (
-    <div>
+    <div className={styles.register}>
       <h1>Register to Post</h1>
       <p>Create your username and share your stories! </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Name: </span>
-          <input type="text" name="name" required placeholder="User Name"></input> 
+          <input type="text" name="name" required placeholder="User Name" value={name} onChange={(e)=> setName(e.target.value)}></input> 
         </label>
         <label>
           <span>Email: </span>
-          <input type="email" name="email" required placeholder="Email"></input> 
+          <input type="email" name="email" required placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)}></input> 
         </label>
         <label>
           <span>Password: </span>
-          <input type="password" name="password" required placeholder="Password"></input> 
+          <input type="password" name="password" required placeholder="Password" value={password} onChange={(e)=> setPassword(e.target.value)}></input> 
         </label>
         <label>
           <span>Confirm Password: </span>
-          <input type="password" name="confirmPassword" required placeholder="Confirm Password"></input> 
+          <input type="password" name="confirmPassword" required placeholder="Confirm Password" value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}></input> 
         </label>
         <label className="btn">Register</label>
       </form>
