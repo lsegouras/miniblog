00acd3f91d2f2c4720170cf1e227a04f2611ae0d
Laura Segouras <lausegouras@gmail.com>
2023-12-21 12:54:32 -0300

    passing context to user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { AuthProvider } from './context/AuthContext';
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from 'react'
+import { useAuthentication } from './hooks/useAuthentication';
+
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    <p>Loading...</p>
+    }
+
   return (
     <div className="App">
       <AuthProvider>
