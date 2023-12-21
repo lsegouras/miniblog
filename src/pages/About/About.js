ee86d441646a25831d4179af1c30b5e09d1fe788
Laura Segouras <lausegouras@gmail.com>
2023-12-21 13:44:14 -0300

    About page html and css

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,12 @@
 import styles from './About.module.css'
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>About the Mini <span>Blog</span></h2>
+      <p>This project consists of a blog coded with React on the front-end and Firebase on the back-end.</p>
+      <Link to="/posts/create" className = "btn">Create Post</Link>
     </div>
   )
 }
