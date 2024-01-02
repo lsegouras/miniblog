ae63c336d504a1197f1b9e92beee1678a1700270
Laura Segouras <lausegouras@gmail.com>
2024-01-02 15:08:35 -0300

    feat create post html and styles

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,49 @@
 import styles from './CreatePost.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthValue } from "../../context/AuthContext"
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
+  const [body, setBody] = useState("")
+  const [tags, setTags] = useState([])
+  const [formError, setFormError] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+      <h2>Create Post</h2>
+      <p>Write about what you would like to share</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Title</span>
+          <input type="text" name="title" required placeholder='Enter your Post Title' onChange={(e) => setTitle(e.target.value)} value={title} />
+        </label>
+        <label>
+          <span>Image URL</span>
+          <input type="text" name="image" required placeholder='Enter your Post Image' onChange={(e) => setImage(e.target.value)} value={image} />
+        </label>
+        <label>
+          <span>Content</span>
+          <textarea name="content" required placeholder='Enter your Post Content' onChange={(e) => setBody(e.target.value)} value={body} />
+        </label>
+        <label>
+          <span>Tags</span>
+          <input type="text" name="tags" required placeholder='Enter your Post Tags, Separated by Commas' onChange={(e) => setTags(e.target.value)} value={tags} />
+        </label>
+        <button className="btn">Publish</button>
+        {/* {!loading && <button className="btn">Register</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Please, wait...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>} */}
+      </form>
     </div>
   )
 }
